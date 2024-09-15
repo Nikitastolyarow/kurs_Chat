ce69a36a1016b932b92594fdaf706fa338bfb179
Nikitastolyarow <nikita.stolyarow2002@yandex.ru>
2024-09-15 11:43:09 +0300

    Реализация требований к клиенту

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,15 +1,17 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import log.logServ;
+
+import java.io.*;
 import java.net.Socket;
+import java.util.Properties;
 import java.util.Scanner;
 
 public class Client {
+    private static int port;
+    private static String host;
     public static void main(String[] args) {
-        String host = "127.0.0.1";
-        int port = 8088;
-
+        loadsettings();
+        System.out.println("Старт сервера с портом " + port);
+        System.out.println("Старт сервера с хостом " + host);
         try (Socket clientSocket = new Socket(host, port);
              PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
              BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())))
@@ -17,13 +19,15 @@ public class Client {
             Scanner scanner = new Scanner(System.in);
             System.out.println("Привет, введи свое имя для подлючения к чату");
             String name = scanner.nextLine();
+            logServ.logLogin(name, "Присоеденился к чату");
             out.println(name);
             System.out.println(in.readLine());
 
             while (true){
-                System.out.println("Введи сообщение или end для выхода из чата");
+                System.out.println("Введи сообщение или exit для выхода из чата");
                 String message = scanner.nextLine();
-                if(message.toLowerCase().equals("end"))
+                logServ.logLogin(name, message);
+                if(message.toLowerCase().equals("exit"))
                     break;
                     out.println(message);
             }
@@ -31,6 +35,17 @@ public class Client {
         }
         catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+    private static void loadsettings()  {
+        Properties properties = new Properties();
+        try (BufferedReader reader = new BufferedReader(new FileReader("settings.txt"))){
+            properties.load(reader);
+            port= Integer.parseInt(properties.getProperty("port"));
+            host= properties.getProperty("host");
+            // System.out.println("Port: " + port);
+        } catch (IOException e) {
+            System.out.println("Ошибка при загрузке файла настроек: " + e.getMessage());
         }
     }
 }
